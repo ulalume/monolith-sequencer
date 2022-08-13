6b5020dac22a9636d8449c7647675c3ff699001e
kbt <kubota@softdevice.co.jp>
2022-08-13 18:28:47 +0900

    change color bits

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 package.path = package.path .. ';' .. love.filesystem.getSource() .. '/lua_modules/share/lua/5.1/?.lua'
 package.cpath = package.cpath .. ';' .. love.filesystem.getSource() .. '/lua_modules/share/lua/5.1/?.so'
 
-local monolith = require "monolith.core".new({ ledColorBits = 9 })
+local monolith = require "monolith.core".new({ ledColorBits = 3 })
 
 local musicSystem
 local soundChanger
